5380792fd9342feedcbbe6dcd80200a9bf170a7a
Matt F <matomatical@gmail.com>
2019-03-24 23:49:57 +1100

    cleaning up the code and added some more substitutions

--- diega_vu.py
+++ diega_vu.py
@@ -1,25 +1,76 @@
 import re
 import random
 
+
 def main():
-    print("Diega Vu: I'm feeling a sense of Deja vu, can YOU feel it too?")
-    you_say = input("Says you: ")
+    print("Diega Vu: I'm feeling a sense of Déjà vu, can YOU feel it too?")
+    you_say = what_do_you_say()
     while you_say:
-        quotes  = segment(transform(you_say.strip()))
-        vu_says = pre()+" "+start()+" "+", and ".join(map(prep, quotes))+" "+end()
+        vu_says = what_does_vu_say(you_say)
         print("Diega Vu: " + vu_says)
-        you_say = input("Says you: ")
-    print("Diega Vu: Oh... Now the feeling has gone... And you too?")
+        you_say = what_do_you_say()
+    print("Diega Vu: Oh... Now the feeling has gone... And you, too?")
+
+def what_do_you_say():
+    try:
+        return input("Says you: ")
+    except EOFError:
+        print("")
+        return "" # signals end of conversation
+def what_does_vu_say(you_say):
+    quotes = segment(transform(you_say.strip()))
+    vu_says = start()+" "+", and ".join(map(prep, quotes))+" "+end()
+    return vu_says
+
+
+SUBSTITUTION_PAIRS = [
+        ("myself", "yourself"),      ("mine", "yours"),     ("my", "your"),
+        ("i'm", "you're"),           ("i am", "you are"),   ("am i", "are you"),
+        ("i was", "you were"),       ("was i", "were you"),
+        ("i wasn't", "you weren't"), ("wasn't i", "weren't you"),
+        ("i", "you"), ("me", "you"), # <-- double-up! prefer you --> i (-/> me)
+    ]
+SUBSTITUTIONS = []
+for _1st_person, _2nd_person in SUBSTITUTION_PAIRS:
+    for find, repl in [(_1st_person, _2nd_person), (_2nd_person, _1st_person)]:
+        SUBSTITUTIONS.append(
+            # look for word(s) matching the pattern in `find`
+            # (surround with r"\b" and replace whitespace with r"\s+")
+            ( re.compile(r"\b" + re.sub(r"\s+", r"\\s+", find) + r"\b")
+            # replace them with the target word `repl`
+            # (uppercased to prevent repeated replacement)
+            , repl.upper()
+            )
+        )
+def transform(reply):
+    """
+    Replace I, me, my, mine, myself, you, yours, your, yourself, i'm, i am,
+    i was, you're, you are, you were, etc. then others like i've and you've, 
+    i'll, you'll, etc. should be caught somewhere within that.
+    """
+    reply = reply.lower()
+    for find, replace in SUBSTITUTIONS:
+        reply = find.sub(replace, reply)
+    reply = reply.upper()
+    return reply
+
+def segment(reply):
+    """naive sentence segmentation on ., !, ?."""
+    sentences = reply.replace(". ", ".|") \
+                     .replace("! ", ".|") \
+                     .replace("? ", "?|") \
+                     .split('|')
+    return sentences
 
 def prep(quote):
-    """naive sentence type detection (handles ?, !, ., and none."""
+    """naive sentence type detection (handles ?, !, ., and none)."""
     if quote.endswith('?'):
         return "asking " + quote.rstrip('.?!')
     else:
         return "saying " + quote.rstrip('.?!')
 
-def pre():
-    return random.choice([
+def start():
+    part1 = random.choice([
         "Oh!",
         "Ah!",
         "Hey!",
@@ -30,52 +81,18 @@ def pre():
         "No way..."
         "Ooh!",
         "Ooh..."])
-def start():
-    return random.choice([
+    part2 = random.choice([
         "I remember, last time, you were",
         "I remember this! You were",
         "It was just like this last time! You were",
         "Just like last time... You were"])
+    return f"{part1} {part2}"
 def end():
     return random.choice([
         "too!",
         "then, too!",
         "back then, too!"])
 
-def segment(reply):
-    """naive sentence segmentation on ., !, ?."""
-    sentences = reply.replace(". ", ".|") \
-                     .replace("! ", ".|") \
-                     .replace("? ", "?|") \
-                     .split('|')
-    return sentences
-
-def transform(reply):
-    """
-    TODO: replace I, me, my mine myself, you, yours your yourself,
-    i'm, i am, i was, you're, you are, you were, etc.
-    then others like i've and you've, i'll, you'll, etc. should be caught
-    somewhere within that
-    """
-    #todo: consider word boundaries using re module
-    reply = reply.lower()
-    reply = re.sub(r"\bmyself\b", "YOURSELF", reply)
-    reply = re.sub(r"\byourself\b", "MYSELF", reply)
-    reply = re.sub(r"\bmine\b", "YOURS", reply)
-    reply = re.sub(r"\byours\b", "MINE", reply)
-    reply = re.sub(r"\bmy\b", "YOUR", reply)
-    reply = re.sub(r"\byour\b", "MY", reply)
-    reply = re.sub(r"\bi'm\b", "YOU'RE", reply)
-    reply = re.sub(r"\bi am\b", "YOU ARE", reply)
-    reply = re.sub(r"\bi was\b", "YOU WERE", reply)
-    reply = re.sub(r"\byou're\b", "I'M", reply)
-    reply = re.sub(r"\byou are\b", "I AM", reply)
-    reply = re.sub(r"\byou were\b", "I WAS", reply)
-    reply = re.sub(r"\bi\b", "YOU", reply)
-    reply = re.sub(r"\bme\b", "YOU", reply)
-    reply = re.sub(r"\byou\b", "I", reply) # or is it me?
-    reply = reply.upper()
-    return reply
 
 if __name__ == '__main__':
     main()
